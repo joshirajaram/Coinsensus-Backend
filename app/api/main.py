b05cfc0d6274ad768b96a0fb3d85d7c41d3f8efd
Rajaram Joshi <rajaram8799@gmail.com>
2024-12-09 04:13:26 -0800

    refactor: minor changes

--- app/api/main.py
+++ app/api/main.py
@@ -4,6 +4,4 @@ from api.routes import users, friends, settlements, transactions
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(friends.router, prefix="/friends", tags=["friends"])
 api_router.include_router(transactions.router, prefix="/transactions", tags=["transactions"])
-api_router.include_router(settlements.router, prefix="/balances", tags=["balances"])
